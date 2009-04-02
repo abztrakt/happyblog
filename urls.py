1d3fa64055638e94a617e839c0a0230a8244221f
Craig M. Stimmel <craig@Wisdom.local>
2009-04-01 23:42:39 -0700

    Added make_object_list so the year archive would get objects.

--- urls.py
+++ urls.py
@@ -11,6 +11,6 @@ urlpatterns = patterns('',
     (r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>\w{1,2})/(?P<slug>[0-9A-Za-z-]+)/$', date_based.object_detail, dict(object_dict, slug_field='slug')),
     (r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>\w{1,2})/$', date_based.archive_day, object_dict),
     (r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/$', date_based.archive_month, object_dict),
-    (r'^(?P<year>\d{4})/$', date_based.archive_year, object_dict),
+    (r'^(?P<year>\d{4})/$', date_based.archive_year, dict(object_dict, make_object_list=True)),
     (r'^/?$', date_based.archive_index, object_dict),
 )
