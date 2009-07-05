334a3161440fe4d43f7a2bb902dea5c274f97e54
Craig M. Stimmel <craig@Wisdom.local>
2009-07-05 14:54:54 -0700

    Support for viewing tag collections and all posts with a specific tag added.

--- urls.py
+++ urls.py
@@ -1,16 +1,23 @@
 from django.conf.urls.defaults import *
-from django.views.generic import date_based
-from abztrakt.happyblog.models import Post
+from django.views.generic import date_based, list_detail
+from abztrakt.happyblog.models import Post, Tag
 
 object_dict = {
     'queryset': Post.objects.all(),
     'date_field': 'date',
 }
 
+tags_dict = {
+    'queryset': Tag.objects.all(),
+}
+
 urlpatterns = patterns('',
+    (r'^tags/(?P<slug>.*)/', 'abztrakt.happyblog.views.tagged_posts', {'posts':Post.objects.all()}),
+    (r'^tags/$', list_detail.object_list, tags_dict),
     (r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>\w{1,2})/(?P<slug>[0-9A-Za-z-]+)/$', date_based.object_detail, dict(object_dict, slug_field='slug')),
     (r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>\w{1,2})/$', date_based.archive_day, object_dict),
     (r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/$', date_based.archive_month, object_dict),
     (r'^(?P<year>\d{4})/$', date_based.archive_year, dict(object_dict, make_object_list=True)),
+    #(r'debug$', 'abztrakt.views.debug', {'debug_in':Post.objects.all()}),
     (r'^/?$', date_based.archive_index, object_dict),
 )
--- views.py
+++ views.py
@@ -1,1 +1,13 @@
 # Create your views here.
+
+from django.shortcuts import render_to_response
+
+def tagged_posts(request, slug, posts):
+    
+    posts_tagged = []
+    for post in posts:
+        for tag in post.assoc_tags.all():
+            if slug == tag.slug:
+                posts_tagged.append(post)
+
+    return render_to_response('happyblog/tagged_posts.html', locals())
