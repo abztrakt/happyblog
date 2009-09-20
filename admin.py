5e13df7e6befa437141d9d2dcb8c602db0076786
Craig M. Stimmel <craig@hackingthesoul.com>
2009-09-19 23:30:00 -0700

    Adding author to posts and autosaving it to the logged in user.

--- admin.py
+++ admin.py
@@ -1,5 +1,16 @@
 from abztrakt.happyblog.models import Post, Tag
 from django.contrib import admin
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    fields = ('slug', 'title', 'date', 'body', 'assoc_tags')
+    list_display = ('title', 'author', 'date')
+    date_hierarchy = 'date'
+
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.author = request.user
+        obj.save()
+
+admin.site.register(Post, PostAdmin)
+
 admin.site.register(Tag)
--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Tag(models.Model):
     slug = models.SlugField(max_length=50)
@@ -10,6 +11,7 @@ class Tag(models.Model):
         return self.slug
 
 class Post(models.Model):
+    author = models.ForeignKey(User)
     slug = models.SlugField()
     title = models.CharField(max_length=256)
     date = models.DateTimeField()
