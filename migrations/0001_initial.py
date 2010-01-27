6d960ac4b1c99d986926f5962a4067e53ccf7a55
Craig M. Stimmel <craig@hackingthesoul.com>
2010-01-26 21:43:14 -0800

    Adopting south for handling model migrations.

---
+++ migrations/0001_initial.py
@@ -0,0 +1,100 @@
+
+from south.db import db
+from django.db import models
+from abztrakt.happyblog.models import *
+
+class Migration:
+    
+    def forwards(self, orm):
+        
+        # Adding model 'Post'
+        db.create_table('happyblog_post', (
+            ('id', orm['happyblog.Post:id']),
+            ('author', orm['happyblog.Post:author']),
+            ('slug', orm['happyblog.Post:slug']),
+            ('title', orm['happyblog.Post:title']),
+            ('date', orm['happyblog.Post:date']),
+            ('body', orm['happyblog.Post:body']),
+        ))
+        db.send_create_signal('happyblog', ['Post'])
+        
+        # Adding model 'Tag'
+        db.create_table('happyblog_tag', (
+            ('id', orm['happyblog.Tag:id']),
+            ('slug', orm['happyblog.Tag:slug']),
+        ))
+        db.send_create_signal('happyblog', ['Tag'])
+        
+        # Adding ManyToManyField 'Post.assoc_tags'
+        db.create_table('happyblog_post_assoc_tags', (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('post', models.ForeignKey(orm.Post, null=False)),
+            ('tag', models.ForeignKey(orm.Tag, null=False))
+        ))
+        
+    
+    
+    def backwards(self, orm):
+        
+        # Deleting model 'Post'
+        db.delete_table('happyblog_post')
+        
+        # Deleting model 'Tag'
+        db.delete_table('happyblog_tag')
+        
+        # Dropping ManyToManyField 'Post.assoc_tags'
+        db.delete_table('happyblog_post_assoc_tags')
+        
+    
+    
+    models = {
+        'auth.group': {
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'unique_together': "(('content_type', 'codename'),)"},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True', 'blank': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'blank': 'True'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'blank': 'True'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'unique_together': "(('app_label', 'model'),)", 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'happyblog.post': {
+            'assoc_tags': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['happyblog.Tag']"}),
+            'author': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"}),
+            'body': ('django.db.models.fields.TextField', [], {}),
+            'date': ('django.db.models.fields.DateTimeField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50', 'db_index': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '256'})
+        },
+        'happyblog.tag': {
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50', 'db_index': 'True'})
+        }
+    }
+    
+    complete_apps = ['happyblog']
